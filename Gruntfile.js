52f885e0319229a199047e1dfb027eaf096d0c08
Steve <sr4136@gmail.com>
2015-06-01 22:35:18 -0400

    gruntfile/package.json/styles updates

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,102 +1,96 @@
 module.exports = function(grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    pkgc: grunt.file.readJSON('package.json'),
+	// 1. All configuration goes here
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
 
-    // Watch files that may change and should trigger updates
-    watch: {
-      sass: {
-        files: ['sass/*.{scss,sass}', 'sass/**/*.scss'],
-        tasks: ['sass', 'autoprefixer', 'uglify'],
-      },
-      js: {
-        files: ['js/app.js'],
-        tasks: ['uglify'],
-      },
-      /*livereload: {
-        options: { livereload: true },
-        files: [
-          '*.php',
-          'inc/*.php',
-          'style.css',
-          'js/app.js',
-          'images/*.{png,jpg,jpeg,gif,webp,svg}',
-        ],
-      }*/
-    },
+		// 2. All functions go here.
+		watch: {
+			grunt: {
+				files: [ 'Gruntfile.js'],
+				options: {
+					reload: true
+				}
+			},
+			/*
+			scripts: {
+				files: [
+				'js-dev/*.js',
+				],
+				tasks: ['concat', 'uglify'],
+				options: {
+					spawn: false,
+				},
+			},
+			*/
+			styles: {
+				files: [
+				'sass/**/*.scss',
+				'sass/*.scss',
+				'sass/style.scss'
+				],
+				tasks: ['sass:dev', 'sass:prod'],
+				options: {
+					spawn: false,
+				}
+			}
+		},
+		/*
+		concat: {
+			scripts: {
+				src: [
+					'js-dev/*.js'
+				],
+				dest: 'js/script.js',
+			}
+		},
+		*/
+		uglify: {
+			scripts: {
+				expand: true,
+				cwd: 'js',
+				src: ['*.js', '!*.min.js'],
+				dest: 'js',
+				ext: '.min.js'
+			},
+			vendor: {
+				expand: true,
+				cwd: 'js/vendor',
+				src: ['**/*.js', '!**/*.min.js'],
+				dest: 'js/vendor',
+				ext: '.min.js'
+			}
+		},
+		sass: {
+			dev: {
+				options: {
+					style: 'expanded',
+					loadPath: 'sass'
+				},
+				files: {
+					'style.css': 'sass/style.scss'
+				}
+			},
+			prod: {
+				options: {
+					style: 'compressed',
+					loadPath: 'sass'
+				},
+				files: {
+					'style.min.css': 'sass/style.scss'
+				}
+			}
+		}
+	});
 
-    // Compile SASS
-    sass: {
-      dist: {
-        options: {
-          style: 'expanded',
-        },
-        files: { // 'destination': 'source' ... obviously.
-          'style.css': 'sass/style.scss',
-        }
-      }
-    },
+	// 3. Where we tell Grunt we plan to use this plug-in.
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-sass');
 
-    // Autoprefix CSS
-    autoprefixer: {
-      options: {
-        browsers: ['last 2 versions', 'ie 8', 'ios 6', 'android 4'],
-        map: true
-      },
-      files: {
-        expand: true,
-        flatten: true,
-        src: 'style.css'
-      },
-    },
+	// 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
+	grunt.registerTask('build', ['sass', 'concat', 'uglify']);
+	grunt.registerTask('default', ['watch']);
 
-    // Uglify to concat, minify, and make source maps
-    uglify: {
-      main: {
-        options: {
-          sourceMap: 'js/main.js.map',
-          sourceMappingURL: 'main.js.map',
-          sourceMapPrefix: 2,
-        },
-        files: {
-          'js/main.min.js': [
-            'js/app.js'
-          ]
-        }
-      }
-    },
-
-	/*
-    // SFTP
-    'sftp-deploy': {
-      build: {
-        auth: {
-          host: 'server.com',
-          port: 22,
-          authKey: 'key1'
-        },
-        cache: 'sftpCache.json',
-        src: '/path/to/source/folder',
-        dest: '/path/to/destination/folder',
-        exclusions: [
-          '/path/to/source/folder/.DS_Store',
-          'dist/tmp'
-        ],
-        serverSep: '/',
-        concurrency: 4,
-        progress: true
-      }
-    },
-	*/
-  });
-
-  // Load all grunt tasks matching the `grunt-*` pattern
-  require('load-grunt-tasks')(grunt);
-
-  // Default task(s)
-  // We can do better! http://gruntjs.com/creating-tasks
-  grunt.registerTask('default', ['watch']);
-  //grunt.registerTask('deploy', ['sftp-deploy']);
-  grunt.registerTask('build', ['sass', 'autoprefixer', 'uglify', 'imagemin']);
-}
+};
